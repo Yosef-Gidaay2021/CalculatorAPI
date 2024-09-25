b00b19702ef4f68cb5a1f49fe452fc38de3227a2
Yosef Giday <A00288051@mycambrian.ca>
2024-09-25 04:35:52 +0000

    API Calculator class created

---
+++ Calculator/Controllers/Calculator.cs
@@ -0,0 +1,12 @@
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace Calculator.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class Calculator : ControllerBase
+    {
+       
+    }
+}
